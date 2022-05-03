7474c16fd8ae39ca9c0592cebbfb61d8ce328a66
Siddharth Sanghavi <sanghavisidd@gmail.com>
2022-05-02 21:46:31 -0500

    Add files via upload

--- stream_test.py
+++ stream_test.py
@@ -1,18 +1,32 @@
-from tracemalloc import start
-from muselsl import stream, list_muses, view, record
-from pylsl import StreamInlet, resolve_stream
+from pylsl import StreamInlet, resolve_stream, resolve_byprop
 import os
+import time
+import numpy as np
+import utils
+import subprocess
+import sys
+
+BUFFER_LENGTH = 20
+EPOCH_LENGTH = 1
+OVERLAP_LENGTH = 0.8
+SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+flag = True
 
 
-def start_stream():
-  streams = resolve_stream('type', 'EEG')
-
-    # create a new inlet to read from the stream
-  inlet = StreamInlet(streams[0])
-
-  while True:
-    sample, timestamp = inlet.pull_sample()
-    print(sample)
+def main():
+    p = subprocess.Popen(["muselsl", "stream"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, encoding="utf-8", errors="replace")
+    
+    while True:
+        output = p.stdout.readline()
+        
+        if output == "" and p.poll() is not None:
+            break
+        if output:
+            print(output)
+        
+        sys.stdout.flush()
+    print("done")
+    
 
 if __name__ == "__main__":
-  start_stream()
+    main()
